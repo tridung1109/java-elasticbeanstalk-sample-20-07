81ef77886a481a268e01077846ed75e44d909d52
tridung1109 <40634573+tridung1109@users.noreply.github.com>
2025-07-20 22:55:13 +0700

    Update HelloController.java

--- src/main/java/com/deploy/aws/demo/controller/HelloController.java
+++ src/main/java/com/deploy/aws/demo/controller/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
     @GetMapping
     public ResponseEntity<?> greetings() {
 
-        return new ResponseEntity<Object>("<h1 style='color:green;'>Congratulations, your app has been deployed!<h1>", HttpStatus.OK);
+        return new ResponseEntity<Object>("<h1 style='color:green;'>Congratulations, your app has been deployed! Beanstalk<h1>", HttpStatus.OK);
     }
 
 }
